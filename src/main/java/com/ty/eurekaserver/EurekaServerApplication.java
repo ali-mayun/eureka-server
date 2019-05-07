c5de4658ad42ed0332d7b1a5906a64e04079ae01
ali-mayun <46866865+ali-mayun@users.noreply.github.com>
2019-05-07 23:54:19 +0800

    eureka-server20190507代码提交
    eureka-server20190507代码提交

---
+++ src/main/java/com/ty/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.ty.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//通过此注解启动一个服务注册中心提供给其他应用进行对话
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
